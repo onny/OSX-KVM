491cdeb4b5f15dc9fd4215eeefc51ea20d8083ba
Joost van der Laan <joostvanderlaan@gmail.com>
2023-10-19 17:15:29 +0200

    WIP fix flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs.nixpkgs.url = "nixpkgs/22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.osx-kvm.url = "path:./";
-  inputs.osx-kvm.flake = false;
+  #inputs.osx-kvm.url = "path:./";
+  #inputs.osx-kvm.flake = false;
+  #PROJECT_ROOT = builtins.toString ./.;
 
-  outputs = { self, nixpkgs, flake-utils, osx-kvm }:
+  outputs = { self, nixpkgs, flake-utils }:
 
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -16,7 +17,7 @@
           pkgs.writeShellScriptBin "start" ''
             set -e
             if [ ! -e BaseSystem.dmg ];then
-              ${pkgs.python3}/bin/python ${osx-kvm}/fetch-macOS-v2.py
+              ${pkgs.python3}/bin/python ./fetch-macOS-v2.py
             fi
             if [ ! -e BaseSystem.img ];then
               ${pkgs.qemu}/bin/qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
@@ -24,7 +25,7 @@
             if [ ! -e mac_hdd_ng.img ];then
               ${pkgs.qemu}/bin/qemu-img create -f qcow2 mac_hdd_ng.img 128G
             fi
-            source ${osx-kvm}/OpenCore-Boot.sh
+            source ./OpenCore-Boot.sh
           '';
       in
       {
